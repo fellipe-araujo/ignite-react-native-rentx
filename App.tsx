e905ccb4f9ba2bd6cc044762e603f197488f08fb
Fellipe <araujo.fellipe@hotmail.com>
2021-10-19 21:20:10 -0300

    Configured fonts and theme.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-import { Home } from './src/screens/Home';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+
+import { CarDetails } from './src/screens/CarDetails';
+import theme from './src/styles/theme';
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CarDetails />
+    </ThemeProvider>
+  )
 }
