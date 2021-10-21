2089322324b4a025d9a0e0824069a960e7ec5a8d
Fellipe <araujo.fellipe@hotmail.com>
2021-10-20 21:39:02 -0300

    Developed Home page.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import { Container, Title } from './styles';
+import { Car } from '../../components/Car';
+
+import Logo from '../../assets/logo.svg';
+
+import { Container, Header, HeaderContent, TotalCars, CarList } from './styles';
 
 export function Home() {
+  const carData = {
+    brand: 'Audi',
+    name: 'Rs 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+  };
+
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <CarList
+        data={[1, 2, 3, 4, 5]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
     </Container>
   );
-};
+}
