4f45c6b9145408995dacd94e52ed2711c9b46f39
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-01-20 18:45:43 -0300

    Developed CarDetails screen.

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
+
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
 
-import { Container, Header, CarImages } from './styles';
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Decription,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  About,
+  Footer,
+} from './styles';
 
 export function CarDetails() {
   return (
@@ -19,6 +43,39 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Decription>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Decription>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasoline" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </Accessories>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado
+          na praça Real Maestranza de Sevilla. É um belíssimo carro para quem
+          gosta de acelerar.
+        </About>
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
     </Container>
   );
 }
