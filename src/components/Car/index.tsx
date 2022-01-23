7bfc4b389571a357c2015958602ba42fa328752c
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-01-23 11:07:57 -0300

    Turned card into button.

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
-import GasolineSvg from '../../assets/gasoline.svg';
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
+import { CarDTO } from '../../dtos/CarDTO';
 
 import {
   Container,
@@ -15,23 +17,15 @@ import {
   CarImage,
 } from './styles';
 
-interface CarData {
-  brand: string;
-  name: string;
-  rent: {
-    period: string;
-    price: number;
-  };
-  thumbnail: string;
+interface Props extends RectButtonProps {
+  data: CarDTO;
 }
 
-interface Props {
-  data: CarData;
-}
+export function Car({ data, ...rest }: Props) {
+  const MotorIcon = getAccessoryIcon(data.fuel_type);
 
-export function Car({ data }: Props) {
   return (
-    <Container>
+    <Container {...rest}>
       <Details>
         <Brand>{data.brand}</Brand>
         <Name>{data.name}</Name>
@@ -43,7 +37,7 @@ export function Car({ data }: Props) {
           </Rent>
 
           <Type>
-            <GasolineSvg />
+            <MotorIcon />
           </Type>
         </About>
       </Details>
