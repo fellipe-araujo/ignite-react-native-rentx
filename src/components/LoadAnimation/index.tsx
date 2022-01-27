d6f21c58f25a380e3238a0c96cd980d73e11be3d
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-01-26 22:19:16 -0300

    Created LoadAnimation component using lottie.

---
+++ src/components/LoadAnimation/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import LottieView from 'lottie-react-native';
+
+import load_animated from '../../assets/load_animated.json';
+
+import { Container } from './styles';
+
+export function LoadAnimation() {
+  return (
+    <Container>
+      <LottieView
+        source={load_animated}
+        style={{ height: 200 }}
+        resizeMode="contain"
+        autoPlay
+        loop
+      />
+    </Container>
+  );
+}
