76d55214a07fbc0586ab1b9ea7def684752e2974
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-02-16 19:40:22 -0300

    Apply algorithm to update profile.

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -3,16 +3,19 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useTheme } from 'styled-components';
 import { Feather } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
+import * as Yup from 'yup';
 
 import { BackButton } from '../../components/BackButton';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
+import { Button } from '../../components/Button';
 
 import { useAuth } from '../../hooks/auth';
 import {
@@ -32,7 +35,7 @@ import {
 } from './styles';
 
 export function Profile() {
-  const { user, signOut } = useAuth();
+  const { user, signOut, updateUser } = useAuth();
 
   const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
   const [avatar, setAvatar] = useState(user?.avatar);
@@ -64,6 +67,35 @@ export function Profile() {
 
     if (result.uri) {
       setAvatar(result.uri);
+    }
+  }
+
+  async function handleUpdateProfile() {
+    try {
+      const schema = Yup.object().shape({
+        driverLicense: Yup.string().required('CNH é obrigatória'),
+        name: Yup.string().required('Nome é obrigatório'),
+      });
+
+      const data = { name, driverLicense };
+      await schema.validate(data);
+      await updateUser({
+        id: user!.id,
+        user_id: user!.user_id,
+        email: user!.email,
+        name: user!.name,
+        driver_license: driverLicense!,
+        avatar: avatar!,
+        token: user!.token,
+      });
+
+      Alert.alert('Perfil atualizado!');
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert('Opa', error.message);
+      } else {
+        Alert.alert('Não foi possível atualizar o perfil');
+      }
     }
   }
 
@@ -137,6 +169,8 @@ export function Profile() {
                 <PasswordInput iconName="lock" placeholder="Repetir senha" />
               </Section>
             )}
+
+            <Button title="Salvar alterações" onPress={handleUpdateProfile} />
           </Content>
         </Container>
       </TouchableWithoutFeedback>
